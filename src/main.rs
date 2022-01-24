7ec2f1dabd026cdcfbc79caef831d9deedd53a23
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-24 21:38:11 +0300

    m

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let parsed_line = env::args().skip(1).collect::<Vec<_>>();
+    let mut sum: usize = 0;
+    for i in parsed_line {
+        for c in i.chars() {
+            sum += sum + c.len_utf8();
+        }
+    }
+    println!("{}", sum);
+
 }
