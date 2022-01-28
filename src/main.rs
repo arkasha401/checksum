7715f5ea2fad76d4f005e7accbf57b29686eb9b7
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-28 23:34:33 +0300

    so

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 
 fn main() {
-    let strofa: &str = "Deutchfrog";
-    let mut xor_sum = 0;
-    let mut add_sum = 0;
-    for i in strofa.chars(){
-        xor_sum = xor_sum + i as u32;
-        add_sum = add_sum ^ i as u32;
-        println!("{} {}", xor_sum, add_sum);
-    }
+    let strofa: &str = "Deutch frog";
 
+    let mut xor_sum8: u8 = 0;
+    let mut add_sum8: u16 = 0;
+    for i in strofa.chars() {
+        xor_sum8 = xor_sum8 ^ i as u8;
+        add_sum8 = add_sum8 + i as u16;
+    }
+    println!("{} {}", xor_sum8, add_sum8)
 }
