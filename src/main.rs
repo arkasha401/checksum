d4a583744aaafec533daba4b5d81858c9a2d4d26
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-30 22:50:23 +0300

    i

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,31 @@ fn main() {
             add_sum = add_sum + i as u16;   
             xor_sum = xor_sum ^ i as u8;
         }
+        println!("{}", add_sum);
         println!("{:>0width$X} {:>0width$X}", add_sum, xor_sum, width = 2);
         if arg.chars().last().unwrap() == '*' {
-            println!("add");
+            if add_sum > 255{
+                add_sum -= 255;
             let d = format!("{}{:X}", arg, add_sum);
             println!("{}", d)
+            } else {
+                let d = format!("{}{:X}", arg, add_sum);
+                println!("{}", d)
+            }
         } else if arg.chars().last().unwrap() == '^' {
-            println!("xor");
-            let d = format!("{}{:X}", arg, add_sum);
-            println!("{}", d)
+            if xor_sum > 255 {
+                xor_sum -= 255;
+                let d = format!("{}{:X}", arg, add_sum);
+                println!("{}", d) 
+            } else {
+                let d = format!("{}{:X}", arg, add_sum);
+                println!("{}", d) 
+            }
         } else {
             eprintln!("Print ^ or * at the end of the line");
             process::exit(1);
             }
         }  
+    
 }    
 
