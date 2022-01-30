464f5b05a818af773f61be55d43d15fe02585200
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-30 22:06:46 +0300

    i

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-
-    let mut xor_sum: u8 = 0;
-    let mut add_sum: u16 = 0;
         for arg in args {
+            let mut xor_sum: u8 = 0;
+            let mut add_sum: u16 = 0;
         for i in arg.chars() {
             add_sum = add_sum + i as u16;   
             xor_sum = xor_sum ^ i as u8;
