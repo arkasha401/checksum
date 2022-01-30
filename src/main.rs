f6916c928ca3cc09e4c648931ba8db36c522c215
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-30 21:49:00 +0300

    v

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,27 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: String = env::args().nth(1).unwrap();
+    let args: Vec<String> = env::args().skip(1).collect();
 
     let mut xor_sum: u8 = 0;
     let mut add_sum: u16 = 0;
-    for i in args.chars() {
-        add_sum = add_sum + i as u16;   
-        xor_sum = xor_sum + i as u8;
-    }
-    println!("{:>0width$X} {:>0width$X}", add_sum, xor_sum, width = 2);
-    if args.chars().last().unwrap() == '*' {
-        println!("add");
-        let d = format!("{}{:x}", args, add_sum);
-        println!("{}", d)
-    } else if args.chars().last().unwrap() == '^' {
-        println!("xor");
-        let d = format!("{}{:x}", args, add_sum);
-        println!("{}", d)
-    } else {
-        eprintln!("Print ^ or * at the end of the line");
-        process::exit(1);
+    for arg in args{
+        for i in arg.chars() {
+            add_sum = add_sum + i as u16;   
+            xor_sum = xor_sum + i as u8;
+        }
+        println!("{:>0width$X} {:>0width$X}", add_sum, xor_sum, width = 2);
+        if arg.chars().last().unwrap() == '*' {
+            println!("add");
+            let d = format!("{}{:x}", arg, add_sum);
+            println!("{}", d)
+        } else if arg.chars().last().unwrap() == '^' {
+            println!("xor");
+            let d = format!("{}{:x}", arg, add_sum);
+            println!("{}", d)
+        } else {
+            eprintln!("Print ^ or * at the end of the line");
+            process::exit(1);
+        }
     }
 }    
