cd170e89225aaf2d48260921384c7652f6b1d852
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-30 15:34:38 +0300

    a

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::process;
 fn main() {
     let args: String = env::args().nth(1).unwrap();
 
-    let mut xor_sum: u16 = 0;
-    let mut add_sum: u16 = 8;
+    let mut xor_sum: u8 = 0;
+    let mut add_sum: u16 = 0;
     for i in args.chars() {
         add_sum = add_sum + i as u16;   
-        xor_sum = xor_sum + i as u16;
+        xor_sum = xor_sum + i as u8;
     }
     println!("{:>0width$X} {:>0width$X}", add_sum, xor_sum, width = 2);
     if args.chars().last().unwrap().to_string() == "*"{
