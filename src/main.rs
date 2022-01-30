5847fcaccf248eb784122f78c707379dafa8fa55
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-30 15:32:31 +0300

    a

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,17 @@ fn main() {
     let args: String = env::args().nth(1).unwrap();
 
     let mut xor_sum: u16 = 0;
-    let mut add_sum: u16 = 0;
-    if args.chars().last().unwrap().to_string() == "*" {
-        for i in args.chars() {
-            add_sum = add_sum + i as u16
+    let mut add_sum: u16 = 8;
+    for i in args.chars() {
+        add_sum = add_sum + i as u16;   
+        xor_sum = xor_sum + i as u16;
         }
+    println!("{:>0width$X} {:>0width$X}", add_sum, xor_sum, width = 2);
+    if args.chars().last().unwrap().to_string() == "*"{
         println!("add")
     } else if args.chars().last().unwrap().to_string() == "^" {
-        for i in args.chars() {
-            xor_sum = xor_sum + i as u16
-        }
         println!("xor")
-
     } else {
-        process::exit(0x0100);
-    }    
-
-
-}
+        process::exit(0x0100)
+    }
+}    
