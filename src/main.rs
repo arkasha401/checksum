378656c3bb573e6c817a7b56d9e4ccf24d3c4b54
arkasha401 <78563188+arkasha401@users.noreply.github.com>
2022-01-30 22:04:01 +0300

    w

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let mut xor_sum: u8 = 0;
     let mut add_sum: u16 = 0;
-    for arg in args{
+        for arg in args {
         for i in arg.chars() {
             add_sum = add_sum + i as u16;   
             xor_sum = xor_sum ^ i as u8;
@@ -14,11 +14,11 @@ fn main() {
         println!("{:>0width$X} {:>0width$X}", add_sum, xor_sum, width = 2);
         if arg.chars().last().unwrap() == '*' {
             println!("add");
-            let d = format!("{}{:x}", arg, add_sum);
+            let d = format!("{}{:X}", arg, add_sum);
             println!("{}", d)
         } else if arg.chars().last().unwrap() == '^' {
             println!("xor");
-            let d = format!("{}{:x}", arg, add_sum);
+            let d = format!("{}{:X}", arg, add_sum);
             println!("{}", d)
         } else {
             eprintln!("Print ^ or * at the end of the line");
